54ccabcf4f44a1ebe1d5fb801ab6ec79efbae239
codtasker <playandroidhb@gmail.com>
2021-06-19 00:49:11 -0300

    adicionado json e modo trabalho

--- classes.py
+++ classes.py
@@ -1,5 +1,12 @@
+from os import times
 import time
 from playsound import playsound
+import json
+
+with open('timer.json') as t:
+    timers = json.load(t)
+
+
 class tempo:
     def timer(timer):
         minu = 0
@@ -18,3 +25,22 @@ class tempo:
         for i in range(r):
             tempo.timer(timer1)
             tempo.timer(timer2)
+
+class trabalho:
+    def alterartimers(nome,t1,t2):
+        timers["times"].append({"i":len(timers[times])+1,"nome":nome,"tempo1":t1,"tempo2":t2})
+        print(timers["times"])
+    def iniciarTimer(i,r):
+        for t in timers["times"]:
+            if i == t["i"]:
+                tempo.timercr(t["tempo1"],t["tempo2"],r)
+    def retornatimer():
+        return timers["times"]
+
+class ultilitarios:
+    def linha():
+        print("<------------------------------------------------------------------------------->")
+
+print(len(timers["times"]))
+print(type(timers["times"]))
+trabalho.alterartimers("normie",1,2)
--- main.py
+++ main.py
@@ -1,14 +1,49 @@
+#importando as classes que vou usar
 import classes as c
-inic = input("qual a opção que voce deseja:\ns = alarme simples\nd = alarme duplo\ndr = alarme duplo com repetição\nR: ")
-if inic == 's':
-    t = int(input('quanto tempo voce quer: '))
-    c.tempo.timer(t)
-elif inic == 'd':
-    t = int(input('digite o tempo 1: '))
-    t2 = int(input('digite o tempo 2: '))
-    c.tempo.timercomparador(t,t2)
-elif inic == 'dr':
-    t = int(input('digite o tempo 1: '))
-    t2 = int(input('digite o tempo 2: '))
-    repet = int(input('digite quantas vezes deve ser repetido: '))
-    c.tempo.timercr(t,t2,repet)
+
+
+#perguntando o modo que o usuario deseja
+while True:
+    op = input('alarme ou modo trabalho(a = alarme t = trabalho): ')
+    if op == "a" or op == "t":break
+
+#modo alarme
+if op == "a":
+    while True:
+        inic = input("qual a opção que voce deseja:\ns = alarme simples\nd = alarme duplo\ndr = alarme duplo com repetição\nR: ")
+        if inic == "s" or inic == "d" or inic == "dr":break
+    c.ultilitarios.linha()
+    if inic == 's':
+        t = int(input('quanto tempo voce quer: '))
+        c.tempo.timer(t)
+    elif inic == 'd':
+        t = int(input('digite o tempo 1: '))
+        t2 = int(input('digite o tempo 2: '))
+        c.tempo.timercomparador(t,t2)
+    elif inic == 'dr':
+        t = int(input('digite o tempo 1: '))
+        t2 = int(input('digite o tempo 2: '))
+        repet = int(input('digite quantas vezes deve ser repetido: '))
+        c.tempo.timercr(t,t2,repet)
+
+#modo trabalho
+else:
+    while True:
+        inic = input("qual a opção que voce deseja:\np = programar nova rotina\na = programados anteriormente\nR: ")
+        if inic == "p" or inic == "a":break
+    c.ultilitarios.linha()
+    if inic == "p":
+        pass
+    else:
+        timers = c.trabalho.retornatimer()
+        print("lista de rotinas")
+        for t in timers:
+            print("[",t["i"],"] nome do timer: "+t["nome"]+" /primeira rotina: ",t["tempo1"]," /segunda rotina: ",t["tempo2"])
+        c.ultilitarios.linha()
+        while True:    
+            try:
+                i = int(input("qual o indice da rotina desejada: "))
+                r = int(input("quantas repeticoes: "))
+                c.trabalho.iniciarTimer(i,r)
+                break
+            except:print("digite novamente (erro)")
