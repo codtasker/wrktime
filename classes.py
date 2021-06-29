3e8b8ecc8a10739400a63257d84428e13397289f
codtasker <playandroidhb@gmail.com>
2021-06-29 17:15:30 -0300

    quality of life

--- classes.py
+++ classes.py
@@ -43,6 +43,21 @@ class trabalho:
 class ultilitarios:
     def linha():
         print("<------------------------------------------------------------------------------->")
-
+    
+    def esctrab():
+        ultilitarios.linha()
+        while True:
+            inic = input("qual a opção que voce deseja:\np = programar nova rotina\ni = iniciar uma rotina pre programada\ns = sair\nR: ")
+            if inic == "p" or inic == "i" or inic == "s":break
+        ultilitarios.linha()
+        return inic
+    
+    def esctimer():
+        ultilitarios.linha()
+        while True:
+            inic = input("qual a opção que voce deseja:\na = alarme simples\nd = alarme duplo\ndr = alarme duplo com repetição\ns = sair\nR: ")
+            if inic == "s" or inic == "d" or inic == "dr" or inic == "a":break
+        ultilitarios.linha()
+        return inic
 
 
--- main.py
+++ main.py
@@ -7,46 +7,50 @@ while True:
     op = input('alarme ou modo trabalho(a = alarme r = rotina): ')
     if op == "a" or op == "r":break
 
+
 #modo alarme
 if op == "a":
+    
     while True:
-        inic = input("qual a opção que voce deseja:\ns = alarme simples\nd = alarme duplo\ndr = alarme duplo com repetição\nR: ")
-        if inic == "s" or inic == "d" or inic == "dr":break
-    c.ultilitarios.linha()
-    if inic == 's':
-        t = int(input('quanto tempo voce quer: '))
-        c.tempo.timer(t)
-    elif inic == 'd':
-        t = int(input('digite o tempo 1: '))
-        t2 = int(input('digite o tempo 2: '))
-        c.tempo.timercomparador(t,t2)
-    elif inic == 'dr':
-        t = int(input('digite o tempo 1: '))
-        t2 = int(input('digite o tempo 2: '))
-        repet = int(input('digite quantas vezes deve ser repetido: '))
-        c.tempo.timercr(t,t2,repet)
+        inic = c.ultilitarios.esctimer()
+
+        if inic == 'a':
+            t = int(input('quanto tempo voce quer: '))
+            c.tempo.timer(t)
+        elif inic == 'd':
+            t = int(input('digite o tempo 1: '))
+            t2 = int(input('digite o tempo 2: '))
+            c.tempo.timercomparador(t,t2)
+        elif inic == 'dr':
+            t = int(input('digite o tempo 1: '))
+            t2 = int(input('digite o tempo 2: '))
+            repet = int(input('digite quantas vezes deve ser repetido: '))
+            c.tempo.timercr(t,t2,repet)
+        else:break
 
 #modo trabalho
 else:
     while True:
-        inic = input("qual a opção que voce deseja:\np = programar nova rotina\ni = iniciar uma rotina pre programada\nR: ")
-        if inic == "p" or inic == "i":break
-    c.ultilitarios.linha()
-    if inic == "p":
-        nome = input("digite o nome da nova rotina: ")
-        t1 = int(input("digite o tempo da primeira tarefa: "))
-        t2 = int(input("digite o tempo da segunda tarefa: "))
-        c.trabalho.alterartimers(nome,t1,t2)
-    else:
-        timers = c.trabalho.retornatimer()
-        print("lista de rotinas")
-        for t in timers:
-            print("[",t["i"],"] nome do timer: "+t["nome"]+" /primeira rotina: ",t["tempo1"]," /segunda rotina: ",t["tempo2"])
-        c.ultilitarios.linha()
-        while True:    
-            try:
-                i = int(input("qual o indice da rotina desejada: "))
-                r = int(input("quantas repeticoes: "))
-                c.trabalho.iniciarTimer(i,r)
-                break
-            except:print("digite novamente (erro)")
+        inic = c.ultilitarios.esctrab()
+
+        if inic == "p":
+            nome = input("digite o nome da nova rotina: ")
+            t1 = int(input("digite o tempo da primeira tarefa: "))
+            t2 = int(input("digite o tempo da segunda tarefa: "))
+            c.trabalho.alterartimers(nome,t1,t2)
+
+        elif inic == "i":
+            timers = c.trabalho.retornatimer()
+            print("lista de rotinas")
+            for t in timers:
+                print("[",t["i"],"] nome do timer: "+t["nome"]+" /primeira rotina: ",t["tempo1"]," /segunda rotina: ",t["tempo2"])
+            c.ultilitarios.linha()
+            while True:    
+                try:
+                    i = int(input("qual o indice da rotina desejada: "))
+                    r = int(input("quantas repeticoes: "))
+                    c.trabalho.iniciarTimer(i,r)
+                    break
+                except:print("digite novamente (erro)")
+
+        else:break
